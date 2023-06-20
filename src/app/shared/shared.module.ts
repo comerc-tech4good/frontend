289048eed001db6f69e4d3ebbb529afd1a8bad94
Ana Sanz <larasanz11@gmail.com>
2023-06-20 13:42:06 +0200

    folding provisional

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { GraphicsComponent } from './components/graphics/graphics.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    LoadingComponent,
+    GraphicsComponent
+  ],
   imports: [
     CommonModule
   ]
