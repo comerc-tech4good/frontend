52230eda14e62df2688ddcd984820260d2536718
Ana Sanz <larasanz11@gmail.com>
2023-06-20 18:53:22 +0200

    graficas

--- src/app/shared/components/graphics/graphics.component.ts
+++ src/app/shared/components/graphics/graphics.component.ts
@@ -37,18 +37,19 @@ export class GraphicsComponent {
   public barChartData: ChartData<'bar'> = {
     labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#21BBDB' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#DBD263'},
+
     ]
   };
 
   // events
   public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
+
   }
 
   public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
+
   }
 
   public randomize(): void {
@@ -64,5 +65,9 @@ export class GraphicsComponent {
 
     this.chart?.update();
   }
+
+
+
+
 
 }
---
+++ src/app/shared/components/two-graphics/two-graphics.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+
+@Component({
+  selector: 'app-two-graphics',
+  templateUrl: './two-graphics.component.html',
+  styleUrls: ['./two-graphics.component.scss']
+})
+export class TwoGraphicsComponent {
+  color: ThemePalette = 'primary';
+  checked = false;
+  disabled = false;
+
+  onRadar(){
+    if(this.checked){
+      this.checked = false;
+    }else{
+      this.checked = true;
+    }
+    console.log(this.checked)
+  }
+
+
+}
