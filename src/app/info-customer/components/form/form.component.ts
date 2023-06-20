72f165afcf01c91d39256d58a015215b4ecf7936
Nacho <francert@hotmail.com>
2023-06-20 20:38:23 +0200

    config implemented

--- src/app/info-customer/components/form/form.component.ts
+++ src/app/info-customer/components/form/form.component.ts
@@ -1,32 +1,41 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ConfigResponse } from 'src/app/api/interfaces';
+import { ConfigApiService } from 'src/app/api/services/config-api.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  config!: ConfigResponse;
 
-  constructor(
-    private fb: FormBuilder
-  ) {
-
+  listOptions!: FormGroup;
+  constructor(private fb: FormBuilder, private configApi: ConfigApiService) {}
+  ngOnInit(): void {
+    this.configApi.get().subscribe((data) => {
+      this.config = data
+      this.listOptions = this.fb.group({
+        activity: new FormControl(this.config.activities, [Validators.required]),
+        parameters: new FormControl(this.config.parameters, [Validators.required]),
+      });
+    });
+    console.log(this.listOptions.value)
   }
-
-  listOptions = this.fb.group({
-    activity: new FormControl(null, [Validators.required]),
-    parameters: new FormControl(null, [Validators.required]),
-  });
-
-  activityList = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'];
-  parametersList = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'];
-
   selected() {
     console.log(this.listOptions.value.activity);
   }
 
-  get activity() { return this.listOptions.get('activity'); }
-  get parameters() { return this.listOptions.get('parameters'); }
-
+  get activity() {
+    return this.listOptions.get('activity');
+  }
+  get parameters() {
+    return this.listOptions.get('parameters');
+  }
 }
